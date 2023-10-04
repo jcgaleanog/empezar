64df49f981c48de369a77260f9f41057c5e8271f
jcgaleanog <44102395+jcgaleanog@users.noreply.github.com>
2023-10-04 11:14:34 -0500

    Cambio

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hola compañeros amigos n/");
+        System.out.printf("Hola compañeros amigos compaaaa n/");
         System.out.printf("POE n/");
 
     }
